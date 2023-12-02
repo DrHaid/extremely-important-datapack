3906ca8a43b9d12d4555d2554105fe981d29faaf
DrHaid <40427630+DrHaid@users.noreply.github.com>
2023-12-02 23:20:11 +0100

    set threshold per player

--- src/ohlawd.ts
+++ src/ohlawd.ts
@@ -1,15 +1,17 @@
-import { _, execute, kill, MCFunction, NBT, Objective, playsound, rel, Selector, setblock, summon, tag } from 'sandstone'
+import { _, execute, kill, MCFunction, NBT, Objective, playsound, rel, Selector, setblock, summon, tag, Variable } from 'sandstone'
 
-const HUNGER_THRESHOLD = 2
+const DEFAULT_THRESHOLD = 20
 const CRUMBS_HELPER_BLOCK = 'minecraft:melon_stem[age=0]'
 const CRUMBS_FRAME_NBT = {Silent: NBT`1b`, Facing: NBT`1b`, Invulnerable: NBT`1b`, Invisible:NBT`1b`, Fixed:NBT`1b`, Tags:["crumbs"], Item:{id:"minecraft:iron_nugget", Count: NBT`1b`, tag:{CustomModelData:344457}}}
 
 const hunger = Objective.create('hunger', 'food', {text: 'Hunger'})
 const prevHunger = Objective.create('prevHunger', 'dummy', {text: 'Previous Hunger'})
 const totalHungerLost = Objective.create('totalHungerLost', 'dummy', {text: 'Total Hunger Lost'})
+const hungerThreshold = Objective.create('hungerThreshold', 'dummy', {text: 'Hunger Threshold'})
 
 MCFunction('init_scoreboard', () => {
   const allPlayers = Selector('@a')
+  hungerThreshold(allPlayers).set(DEFAULT_THRESHOLD)
   totalHungerLost(allPlayers).set(0)
   prevHunger(allPlayers).set(hunger(allPlayers))
 }, {
@@ -22,6 +24,7 @@ MCFunction('check_hunger', () => {
     const pHunger = hunger(player)
     const pPrevHunger = prevHunger(player)
     const pTotalHungerLost = totalHungerLost(player)
+    const pHungerThreshold = totalHungerLost(player)
 
     // check if hunger has changed
     _.if(pHunger.lessThan(pPrevHunger), () => {
@@ -30,7 +33,7 @@ MCFunction('check_hunger', () => {
     })
     
     // do something if threshold reached
-    _.if(pTotalHungerLost.greaterOrEqualThan(HUNGER_THRESHOLD), () => {
+    _.if(pTotalHungerLost.greaterOrEqualThan(pHungerThreshold), () => {
       pTotalHungerLost.set(0)
       placeItemFrame()
     })
