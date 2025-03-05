d1fffc19e1b352599804eda7d47cbb91939afad3
DrHaid <40427630+DrHaid@users.noreply.github.com>
2025-03-05 21:09:46 +0100

    updated pack format

--- sandstone.config.ts
+++ sandstone.config.ts
@@ -3,7 +3,7 @@ import type { SandstoneConfig } from 'sandstone'
 export default {
   name: 'extremely-important-datapack',
   description: [ 'An ', { text: 'extemely important', color: 'gold' }, ' data pack.' ],
-  formatVersion: 42,
+  formatVersion: 61,
   namespace: 'extremely-important-datapack',
   packUid: 'ZKBfOwKq',
   saveOptions: { path: './.sandstone/output/datapack' },
